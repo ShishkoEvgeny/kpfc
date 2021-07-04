3ab60a3bc34668334775656f4c42a041fb1a9f35
ShishkoEvgeny <59554681+ShishkoEvgeny@users.noreply.github.com>
2021-07-04 19:20:29 +0700

    Update kpfc_main.py

--- kpfc_main.py
+++ kpfc_main.py
@@ -13,40 +13,45 @@ def cyrillic(text):
     return bool(re.search('[а-я]', text))
 
 
-ing = input('ingredient ')      ### Ввод ингредиента
 
-try:
-    ing = input('ingredient ')
-    test = cyrillic(ing)
-    if test == True:
-        with open('data_kpfc.txt', encoding='utf-8') as book:
-            x = book.read()
-            s = x.split('=')
-            a = [i for i in range(len(s)) if ing in s[i]]
-            print(a)
-            jtr = a[0]
-            new = [s[jtr]]
-            kpfch = (new[0])
-    else:
-        print('no numbers, no english, only cyrillic!')
-except IndexError:
-    print('absent value')
 
-#if ing == 'курица':
-#    kpfch = {'kcal': '113', 'proteins': '24', 'fats': '1.9', 'carbohydrates': '0.4'}
+while True:
+    try:
+        #ing = input('ingredient ')      ### Ввод ингредиента
+        ing = 'курица'
+        test = cyrillic(ing)
+        if test == True:
+            with open('data_kpfc.txt', encoding='utf-8') as book:
+                x = book.read()
+                s = x.split('=')
+                a = [i for i in range(len(s)) if ing in s[i]]
+                jtr = a[0]
+                new = [s[jtr]]
+                kpfch = (new[0])
+                break
+        else:
+            print('no numbers, no english, only cyrillic!')
+    except IndexError:
+        print('absent value')
 
-k = float(kpfch['kcal'])
-p = float(kpfch['proteins'])
-f = float(kpfch['fats'])
-c = float(kpfch['carbohydrates'])
+f = kpfch.split('+')
+print('product - ', f[0])
+kcal = float(f[1])
+proteins = float(f[2])
+fats = float(f[3])
+carbohydrates = float(f[4])
+
+print('kcal on 100g - ', kcal)
+print('proteins on 100g - ', proteins)
+print('fats on 100g - ', fats)
+print('carbohydrates on 100g - ', carbohydrates)
 
 tw = float(input('total weight: '))
-tk = total(k, tw)
-tp = total(p, tw)
-tf = total(f, tw)
-tc = total(c, tw)
+kcal = total(kcal, tw)
+proteins = total(proteins, tw)
+fats = total(fats, tw)
+carbohydrates = total(carbohydrates, tw)
 
-kpfct = {'kcal': tk, 'proteins': tp, 'fats': tf, 'carbohydrates': tc}
+kpfct = {'kcal': kcal, 'proteins': proteins, 'fats': fats, 'carbohydrates': carbohydrates}
 
 print(kpfct)
-
